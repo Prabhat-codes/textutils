3e714925e315a81d064c22b83bca9b724d474576
Prabhat <prabhat.namdharani@learner.manipal.edu>
2023-03-19 11:28:33 +0530

    Testing out git

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <h1>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe aut excepturi vero, 
           earum quo iusto assumenda eveniet et non perferendis quis commodi, inventore quidem mollitia 
-          voluptatem, in accusantium amet accusamus.
+          voluptatem, in accusantium amet accusamus. Testing Git out
         </h1>
       </div>
     </div>
